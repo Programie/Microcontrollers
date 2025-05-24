bbeac373bc09b1f9e329e63fe42e7f916367a46a
Michael Wieland <git@selfcoders.com>
2025-05-24 21:11:50 +0200

    Add ButtonHandler to handle iterrupts for buttons

--- common/utils.py
+++ common/utils.py
@@ -24,3 +24,29 @@ class PinManager:
         pin.on()
         await asyncio.sleep(duration)
         pin.off()
+
+class ButtonHandler:
+    def __init__(self, callback = None, callback_pressed = None, callback_released = None):
+        self.old_value = None
+        self.callback = callback
+        self.callback_pressed = callback_pressed
+        self.callback_released = callback_released
+
+    def register_irq(self, port: int):
+        Pin(port, Pin.IN, Pin.PULL_UP).irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self.handler)
+
+    def handler(self, pin):
+        value = pin.value()
+
+        if self.old_value is None or value != self.old_value:
+            self.old_value = value
+
+            if value == 0:
+                if self.callback_pressed:
+                    self.callback_pressed()
+            else:
+                if self.callback_released:
+                    self.callback_released()
+
+            if self.callback:
+                self.callback()
